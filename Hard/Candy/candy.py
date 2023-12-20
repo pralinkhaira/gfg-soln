84692af4a60d9e8814f1ec7c5940da2828a4bfd6
BitH0xer <be.coder.19@gmail.com>
2023-12-21 00:34:19 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Candy/candy.py
@@ -0,0 +1,35 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+#User function Template for python3
+
+class Solution:
+    def minCandy(self, N, ratings):
+        candies = [1] * N
+        
+        for i in range(1, N):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+        
+        for i in range(N - 2, -1, -1):
+            if ratings[i] > ratings[i + 1] and candies[i] <= candies[i + 1]:
+                candies[i] = candies[i + 1] + 1
+        
+        return sum(candies)
+
+
+
+#{ 
+ # Driver Code Starts.
+if __name__ == '__main__': 
+    t = int(input ())
+    for _ in range (t):
+        N = int(input())
+        ratings = list(map(int, input().split()))
+        ob = Solution()
+        res = ob.minCandy(N, ratings)
+        print(res)
+# } Driver Code Ends
