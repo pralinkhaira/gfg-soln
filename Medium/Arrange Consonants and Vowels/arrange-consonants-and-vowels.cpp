3a2389eed6eb6d059bca449636616c75e34b73c7
BitH0xer <be.coder.19@gmail.com>
2024-05-01 18:45:28 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Arrange Consonants and Vowels/arrange-consonants-and-vowels.cpp
@@ -0,0 +1,109 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    char data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printlist(Node *head) {
+    if (head == NULL)
+        return;
+    while (head != NULL) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+void append(struct Node **headRef, char data) {
+    struct Node *new_node = new Node(data);
+    struct Node *last = *headRef;
+
+    if (*headRef == NULL) {
+        *headRef = new_node;
+        return;
+    }
+    while (last->next != NULL)
+        last = last->next;
+    last->next = new_node;
+    return;
+}
+
+
+// } Driver Code Ends
+/*
+Structure of the node of the linked list is as
+struct Node
+{
+    char data;
+    struct Node *next;
+
+    Node(char x){
+        data = x;
+        next = NULL;
+    }
+
+};
+*/
+
+class Solution {
+  public:
+    // task is to complete this function
+    // function should return head to the list after making
+    // necessary arrangements
+    struct Node* arrangeCV(Node* head) {
+        // Code here
+        Node* dummy = new Node('a');
+        Node* dummy2 = new Node('a');
+        dummy2->next = head;
+        Node* temp = dummy;
+        Node* prev = dummy2;
+        Node* curr = head;
+        while(curr){
+            while(curr && (curr->data=='a' || curr->data=='e' || curr->data=='i' || curr->data=='o' || curr->data=='u')){
+                temp->next = curr;
+                curr = curr->next;
+                temp = temp->next;
+                temp->next = NULL;
+            }
+            prev->next = curr;
+            prev = curr;
+            if(curr)
+            curr = curr->next;
+        }
+        temp->next = dummy2->next;
+        return dummy->next;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// task is to complete this function
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        char tmp;
+        struct Node *head = NULL;
+        cin >> n;
+        while (n--) {
+            cin >> tmp;
+            append(&head, tmp);
+        }
+        Solution obj;
+        head = obj.arrangeCV(head);
+        printlist(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
