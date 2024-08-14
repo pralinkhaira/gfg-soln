2e888c53f96d4b8c59635ff31be11bbe4fb87cb9
BitH0xer <be.coder.19@gmail.com>
2024-08-15 01:44:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.cpp
@@ -0,0 +1,104 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->data % 10;
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+
+// } Driver Code Ends
+// User function template for C++
+
+/*
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution {
+  public:
+    int addHelper(Node* head){
+        if(!head) return 1;
+       
+        int val =head->data+addHelper(head->next);
+        head->data=val%10;
+        return val/10;
+    }
+    Node* addOne(Node* head) {
+        // Your Code here
+        // return head of list after adding one
+        
+        
+        if(addHelper(head))
+        {
+           Node*newNode= new Node(1);
+           newNode->next=head;
+           return newNode;
+        }
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        if (arr.empty()) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        int data = arr[0];
+        struct Node* head = new Node(data);
+        struct Node* tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            data = arr[i];
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        Solution ob;
+        head = ob.addOne(head);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
