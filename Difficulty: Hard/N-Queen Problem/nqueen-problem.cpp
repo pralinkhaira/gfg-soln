48639bc22852c6fb0bbd9b0b5fe45d90b5b23891
BitH0xer <be.coder.19@gmail.com>
2025-01-30 15:46:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
+++ Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
@@ -4,87 +4,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 // User function Template for C++
 
-class Solution{
-public:
-    void placeNQueen(vector<vector<int>>& ans,
-                     vector<int>& sol,
-                     int row,int n,
-                     vector<bool>& columns_record,
-                     vector<bool>& left_diagonal_record,
-                     vector<bool>& right_diagonal_record){
-        
-        
-        //base case
-        if(row==n){
-            ans.push_back(sol);
+class Solution {
+  public:
+    vector<vector<int>> nQueen(int n) {
+       
+        vector<vector<int>> result;
+        vector<int> board(n, 0);
+        solve(0, n, board, result);
+        return result;
+    }
+
+private:
+    void solve(int col, int n, vector<int>& board, vector<vector<int>>& result) {
+        if (col == n) {
+            result.push_back(board);
             return;
         }
         
-        
-        //iterate over all columns
-        for(int col=0;col<n;col++){
-            if(columns_record[col]==false &&
-               left_diagonal_record[row-col+n-1]==false &&
-               right_diagonal_record[row+col]==false
-            ){
-                columns_record[col] = true;
-                left_diagonal_record[row-col+n-1] = true;
-                right_diagonal_record[row+col]=true;
-                sol.push_back(col+1);
-                placeNQueen(ans,sol,row+1,n,columns_record,left_diagonal_record,right_diagonal_record);
-                columns_record[col] =  left_diagonal_record[row-col+n-1] = right_diagonal_record[row+col]=false;
-                sol.pop_back();
-                
+        for (int row = 0; row < n; row++) {
+            if (isSafe(board, col, row)) {
+                board[col] = row + 1;
+                solve(col + 1, n, board, result);
             }
         }
     }
-    vector<vector<int>> nQueen(int n) {
+    
+    bool isSafe(vector<int>& board, int col, int row) {
+        for (int i = 0; i < col; i++) {
+            int placedRow = board[i] - 1;
+            if (placedRow == row || abs(placedRow - row) == abs(i - col)) {
+                return false;
+            }
+        }
+        return true;
+  
+
         // code here
-        vector<vector<int>> ans;
-        
-        //create a vector to store every possible ans
-        vector<int> sol;
-        
-        vector<bool> columns_record(n,false),
-                     left_diagonal_record((2*n) -1 ,false),
-                     right_diagonal_record((2*n) -1 ,false);
-                     
-        
-        placeNQueen(ans,sol,0,n,columns_record,left_diagonal_record,right_diagonal_record);
-        
-        return ans;
-        
     }
 };
 
-
-
-
 //{ Driver Code Starts.
 
-int main(){
+int main() {
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--) {
         int n;
-        cin>>n;
-        
+        cin >> n;
+
         Solution ob;
         vector<vector<int>> ans = ob.nQueen(n);
-        if(ans.size() == 0)
-            cout<<-1<<"\n";
+        if (ans.size() == 0)
+            cout << -1 << "\n";
         else {
-            for(int i = 0;i < ans.size();i++){
-                cout<<"[";
-                for(int u: ans[i])
-                    cout<<u<<" ";
-                cout<<"] ";
+            sort(ans.begin(), ans.end());
+            for (int i = 0; i < ans.size(); i++) {
+                cout << "[";
+                for (int u : ans[i])
+                    cout << u << " ";
+                cout << "] ";
             }
-            cout<<endl;
+            cout << endl;
         }
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
