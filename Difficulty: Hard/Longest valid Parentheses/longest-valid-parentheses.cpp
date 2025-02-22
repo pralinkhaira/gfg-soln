8745a9adb8b89dd0064cd7118ce05ca8cbbdeb43
BitH0xer <be.coder.19@gmail.com>
2025-02-22 14:33:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.cpp
+++ Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.cpp
@@ -6,25 +6,44 @@ using namespace std;
 
 
 // } Driver Code Ends
-// User function Template for C++
 
 class Solution {
   public:
-    int maxLength(string s) {
-        // code here
-        stack<int> stk;
-        stk.push(-1);
-        int count=0;
-        for (int i=0;i<s.length();i++){
-            if(s[i]=='(') stk.push(i);
-            else{
-                stk.pop();
-                if(stk.empty()) stk.push(i);
-                int ans=i-stk.top();
-                count=max(count,ans);
+    int maxLength(string& s) {
+    stack<int> st;
+
+    // Push -1 as the initial index to 
+      // handle the edge case
+    st.push(-1);
+    int maxLen = 0;
+
+    // Traverse the string
+    for (int i = 0; i < s.length(); i++) {
+        
+        // If we encounter an opening parenthesis,
+          // push its index
+        if (s[i] == '(') {
+            st.push(i);
+        } else {
+            
+            // If we encounter a closing parenthesis,
+              // pop the stack
+            st.pop();
+
+            // If stack is empty, push the current index 
+            // as a base for the next valid substring
+            if (st.empty()) {
+                st.push(i);
+            } else {
+                
+                // Update maxLength with the current length 
+                // of the valid parentheses substring
+                maxLen = max(maxLen, i - st.top());
             }
         }
-        return count;
+    }
+
+    return maxLen;
     }
 };
 
@@ -39,6 +58,9 @@ int main() {
 
         Solution ob;
         cout << ob.maxLength(S) << "\n";
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
