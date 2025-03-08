bd53bb3e0a272b350e13f41324ea39a0b2221737
BitH0xer <be.coder.19@gmail.com>
2025-03-08 20:49:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Palindrome in a String/longest-palindrome-in-a-string.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    string longestPalindrome(string &s) {
+        // code here
+        if (s.empty()) return "";  
+    int start = 0, maxLen = 1;
+
+    auto expandAroundCenter = [&](int left, int right) {
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            if (right - left + 1 > maxLen) {
+                start = left;
+                maxLen = right - left + 1;
+            }
+            left--;
+            right++;
+        }
+    };
+
+    for (int i = 0; i < s.length(); i++) {
+        expandAroundCenter(i, i);   
+        expandAroundCenter(i, i + 1); 
+    }
+
+    return s.substr(start, maxLen);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S;
+
+        Solution ob;
+        cout << ob.longestPalindrome(S) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+}
+
+// } Driver Code Ends
