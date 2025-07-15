785b761298dbb2afd2a4296291888789189065a5
BitH0xer <be.coder.19@gmail.com>
2025-07-15 22:43:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Divisible by 13/divisible-by-13.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    bool divby13(string &s) {
+        // code here
+        int rem=0;
+        // 48-57
+        for(int i=0 ; i<s.length() ; i++){
+            if(rem<13){
+                rem=rem*10+(s[i]-48);
+            }
+            rem=rem%13;
+        }
+        return rem==0;
+    }
+};
